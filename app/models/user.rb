0d21156751d4a82387fb71f6cbcab851be576b8c
Anthony Cook <antrafcook@gmail.com>
2018-04-05 21:20:20 -0400

    created models for users, courses, enrollments, convos, messages. Created posts scaffold but didn't migrate

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  has_many :enrollments
+
+  has_many :messages, dependent: :destroy
+
+  has_many :authored_conversations,
+   class_name: 'Conversation', foreign_key: 'author_id'
+  has_many :received_conversations, class_name: 'Conversation', foreign_key: 'received_id'
+
+  before_save { self.email = email.downcase }
+
+  valid_email = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates(:name, presence: true, length: { minimum: 2, maximum: 25  })
+
+  validates(:email, presence: true, format: { with: valid_email }, length: {minimum: 5, maximum: 25}, uniqueness: { case_sensitive: false})
+
+  validates :password_digest, presence: true, length: { minimum: 6 }
+
+  has_secure_password
+end
