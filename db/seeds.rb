a63c598f78c1daa7051e54d074c733de2707203f
Cora Quezada <corafenice1013@gmail.com>
2018-04-07 19:21:10 -0400

    addeds users and courses to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,77 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+User.create!([{
+  name:"Sally Tomkins",
+  email: "s.tomkins@abc.edu",
+  password: "sally",
+  password_confirmation: "sally"
+},
+{
+  name:"Harry Johnson",
+  email: "h.johnson@abc.edu",
+  password: "harry",
+  password_confirmation: "harry"
+},
+{
+  name:"Gita Patel",
+  email: "g.patel@abc.edu",
+  password: "gita",
+  password_confirmation: "gita"
+},
+{
+  name:"Lauren Hill",
+  email: "l.hill@abc.edu",
+  password: "lauren",
+  password_confirmation: "lauren"
+},
+{
+  name:"Bob Liston",
+  email: "bob.liston@abc.edu",
+  password: "bob",
+  password_confirmation: "bob"
+  }])
+
+  p "Created #{User.count} users"
+
+
+  Course.destroy_all
+
+  Course.create!([{
+    name:"Math",
+    grade: "3rd",
+    school_year: "2018",
+    semester: "1",
+    period: "2"
+  },
+  {
+    name:"English",
+    grade: "3rd",
+    school_year: "2018",
+    semester: "1",
+    period: "1"
+  },
+  {
+    name:"Science",
+    grade: "3rd",
+    school_year: "2018",
+    semester: "2",
+    period: "2"
+  },
+  {
+    name:"Social Studies",
+    grade: "3rd",
+    school_year: "2018",
+    semester: "3",
+    period: "3"
+  },
+  {
+    name:"Music",
+    grade: "3rd",
+    school_year: "2018",
+    semester: "4",
+    period: "1"
+    }])
