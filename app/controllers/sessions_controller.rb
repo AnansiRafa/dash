a89def8dafc14c4d3709aed45594c424835c423d
Anthony Cook <antrafcook@gmail.com>
2018-04-09 17:15:23 -0400

    last commit before trying new association for feedback

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       log_in user
       redirect_to root_url
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:alert] = 'Invalid email/password combination'
       render 'new'
     end
   end
