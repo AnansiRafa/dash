9f9481f65f0d4aaf820e382d3d4639ba0a171293
Your Name <you@example.com>
2018-04-11 16:11:15 -0400

    changes to comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,6 @@ class Post < ApplicationRecord
   has_many :categories, through: :post_categories
   has_many :post_comments
   has_many :comments, through: :post_comments
-=======
->>>>>>> 8a536953aef8c96fecc8aaf1df5b3bd7f0013330
 
   validates :title, presence: true,
                 length: { minimum: 4, maximum: 20 }
