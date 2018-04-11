682916cd4320d7c42f623927fcd267935f6902e7
Anthony Cook <antrafcook@gmail.com>
2018-04-11 16:04:59 -0400

    removed student name and key from enrollments

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,3 +2,4 @@ class Course < ApplicationRecord
     has_many :enrollments, dependent: :destroy
     has_many :posts, dependent: :destroy
     has_many :students, through: :enrollments
+  end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411195219) do
+ActiveRecord::Schema.define(version: 20180411200357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,8 +42,8 @@ ActiveRecord::Schema.define(version: 20180411195219) do
     t.integer "role_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "key"
-    t.string "student_name"
+    t.bigint "student_id"
+    t.index ["student_id"], name: "index_enrollments_on_student_id"
   end
 
   create_table "feedbacks", force: :cascade do |t|
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 20180411195219) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "enrollments", "students"
   add_foreign_key "messages", "conversations"
   add_foreign_key "messages", "users"
   add_foreign_key "posts", "courses"
