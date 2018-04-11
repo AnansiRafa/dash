b7f7dcc92ed56ab7e55977877d4f1ecccdc608cb
Cora Quezada <corafenice1013@gmail.com>
2018-04-11 17:52:06 -0400

    added file upload to view

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,9 +1,5 @@
 class Course < ApplicationRecord
     has_many :enrollments, dependent: :destroy
     has_many :posts, dependent: :destroy
-<<<<<<< HEAD
-end
-=======
     has_many :students, through: :enrollments
   end
->>>>>>> c31f700ca5b33c16dd406c00654fe65e10baa8f7
