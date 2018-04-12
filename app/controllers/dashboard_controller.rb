fd840e5f8fc0cca84bd7fe2613c2b427afa7c344
Anthony Cook <antrafcook@gmail.com>
2018-04-12 15:22:57 -0400

    starting to individualize dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
   def index
+    @student = Student.find(params[:student_id])
     @courses = current_user.courses
     @conversations = Conversation.participating(current_user).order('updated_at DESC')
     @feedbacks = current_user.received_feedbacks
