eadfb37f2bc9994875fe8d80adfe956dce387dcf
Cora Quezada <corafenice1013@gmail.com>
2018-04-12 19:50:06 -0400

    removed merger lines

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -2,6 +2,7 @@ module DashboardHelper
 
   def get_feedbacks(student)
     student.enrollments.map do |e|
+      #DO NOT UNCOMMENT
       # return e.received_feedbacks
       # expecting a string back, given an array
     end
