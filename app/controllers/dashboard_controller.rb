e6d14b9ceca5b604ca609da523c9fff7758da939
Anthony Cook <antrafcook@gmail.com>
2018-04-12 16:00:16 -0400

    personalized dashboard appears functional

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,14 @@
 class DashboardController < ApplicationController
+  include DashboardHelper
+  before_action :get_student, only: [:index]
   def index
-    @student = Student.find(params[:student_id])
+    @feedbacks = get_feedbacks(@student)
     @courses = current_user.courses
     @conversations = Conversation.participating(current_user).order('updated_at DESC')
-    @feedbacks = current_user.received_feedbacks
+  end
+
+  private
+  def get_student
+    @student = Student.find(params[:student_id])
   end
 end
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,8 @@
 module DashboardHelper
+
+  def get_feedbacks(student)
+    student.enrollments.map do |e|
+      return e.received_feedbacks
+    end
+  end
 end
