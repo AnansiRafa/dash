47a1a2a28a8c739bee451cf0aa3c2a280fa5d860
Anthony Cook <antrafcook@gmail.com>
2018-04-24 09:24:25 -0400

    removed parallax

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -1,8 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$( document ).on('turbolinks:load', function() {
-  // $('.dropdown-trigger').dropdown();
+$(document).on('turbolinks:load', function() {
   $(".button-collapse").sideNav();
     $('select').attr("class", "browser-default")
-    $('.parallax').parallax();
 });
