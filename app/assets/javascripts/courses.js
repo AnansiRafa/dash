1666c8dc4f269331e2c4a50633695cd652adb6a5
Anthony Cook <antrafcook@gmail.com>
2018-04-30 09:26:02 -0400

    remove profanity

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -1,7 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).on('turbolinks:load', function() {
-  console.log("Where my niggas at")
   $(".button-collapse").sideNav();
     $('select').attr("class", "browser-default")
 });
